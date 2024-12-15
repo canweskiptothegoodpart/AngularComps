5c80608b2d4b06cea171f6f8cb3d51c7db32684f
Norayr Sukiasyan <norayr_sukiasyan@edu.aua.am>
2024-12-15 22:51:33 +0400

    initial commit

---
+++ src/app/collections/collection-home/collection-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-home',
+  standalone: false,
+  
+  templateUrl: './collection-home.component.html',
+  styleUrl: './collection-home.component.css'
+})
+export class CollectionHomeComponent {
+
+}
